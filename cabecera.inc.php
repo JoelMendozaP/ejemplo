a41d2e3b0cde264dba2c457ed115d851e7c94813
JoelMendozaP <joelmendozapaucara@gmail.com>
2020-09-22 09:49:20 -0400

    Tarea 1 terminada

---
+++ cabecera.inc.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Ejemplo</title>
+		<link href="/ejemplo/hojadeestilos.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div id="cabecera">
+		Facultad de Cs. Puras y Naturales
+		</div>
+		<div id="menu">
+			<h1>
+			Menu
+			</h1>
+			<a href="/ejemplo">INICIO</a><br>
+			Visite alguna carrera:<br>
+			<a href='/ejemplo/informatica'>Informatica</a><br>
+			<a href='/ejemplo/informatica/instituto/'>III</a><br>
+			<a href='/ejemplo/fisica/'>fisica</a><br>
+		</div>
+
+		<div class="cuerpo">
---
+++ fisica/index.php
@@ -0,0 +1,22 @@
+<?php
+include "../cabecera.inc.php";
+?>
+			<a href='google.com'>Google</a><br>
+			<a href='www.google.com'>Google</a><br>
+			<a href='http://google.com'>Google</a><br>
+			Datos<br>
+			<table border="1">
+				<tr>
+					<td>1</td>
+					<td>2</td>
+					<td>3</td>
+				<tr>
+				<tr>
+					<td>4</td>
+					<td>5</td>
+					<td>6</td>
+				<tr>
+			</table>
+<?php
+include "../pie.inc.php";
+?>
